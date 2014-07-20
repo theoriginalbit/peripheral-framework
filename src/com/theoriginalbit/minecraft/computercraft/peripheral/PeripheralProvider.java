992f4b09d14ed40a6fc79f2ce991b99d349afe89
theoriginalbit <theoriginalbit@me.com>
2014-07-20 22:16:37 +1000

    improved PeripheralProvider with better logic and cache
    PeripheralProvider now has a WeakHashMap cache, as well as better validation being performed when attemping to wrap peripherals, throwing exceptions when the developer has implemented something wrong

--- src/com/theoriginalbit/minecraft/computercraft/peripheral/PeripheralProvider.java
+++ src/com/theoriginalbit/minecraft/computercraft/peripheral/PeripheralProvider.java
@@ -1,5 +1,7 @@
 package com.theoriginalbit.minecraft.computercraft.peripheral;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
 import com.theoriginalbit.minecraft.computercraft.peripheral.annotation.LuaPeripheral;
 import com.theoriginalbit.minecraft.computercraft.peripheral.wrapper.PeripheralWrapper;
 
@@ -7,6 +9,8 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import dan200.computercraft.api.peripheral.IPeripheralProvider;
+
+import java.util.WeakHashMap;
 
 /**
  * Peripheral Framework is an open-source framework that has the aim of
@@ -43,22 +47,50 @@ import dan200.computercraft.api.peripheral.IPeripheralProvider;
  */
 public final class PeripheralProvider implements IPeripheralProvider {
 
+    private final WeakHashMap<TileEntity, PeripheralWrapper> PERIPHERAL_CACHE = new WeakHashMap<TileEntity, PeripheralWrapper>();
+
+    /**
+     * Provide ComputerCraft with an IPeripheral wrapper implementation
+     * of a Peripheral-Framework peripheral.
+     */
 	@Override
 	public final IPeripheral getPeripheral(World world, int x, int y, int z, int side) {
 		TileEntity tile = world.getBlockTileEntity(x, y, z);
 
-        Object target = null;
-        if (Utils.isAnnotated(tile, LuaPeripheral.class)) {
-            target = tile;
-        } else if (tile instanceof ILuaPeripheralProvider) {
-            target = ((ILuaPeripheralProvider) tile).getPeripheral();
+        // check if there is a cached peripheral for this TileEntity
+        if (PERIPHERAL_CACHE.containsKey(tile)) {
+            // return the cached value
+            return PERIPHERAL_CACHE.get(tile);
         }
 
-		if (target != null) {
-			return new PeripheralWrapper(target);
-		}
+        // the potential peripheral wrapper
+        PeripheralWrapper wrapper = null;
 
-		return null;
+        // does the TileEntity specify that it provides an external peripheral
+        if (tile instanceof ILuaPeripheralProvider) {
+            // it was an ILuaPeripheralProvider, why is there a LuaPeripheral annotation present?
+            Preconditions.checkArgument(!isLuaPeripheral(tile), "Peripherals cannot implement ILuaPeripheralProvider and have the LuaPeripheral annotation present");
+            // get the peripheral from the ILuaPeripheralProvider
+            final Object peripheral = ((ILuaPeripheralProvider) tile).getPeripheral();
+            // make sure the provided peripheral is annotated
+            Preconditions.checkArgument(isLuaPeripheral(peripheral), "The peripheral returned from the ILuaPeripheralProvider was not annotated with LuaPeripheral");
+            // wrap the return
+            wrapper = new PeripheralWrapper(peripheral);
+        // if the TileEntity is annotated as a LuaPeripheral
+        } else if (isLuaPeripheral(tile)) {
+            wrapper = new PeripheralWrapper(tile);
+        }
+
+        // if there is a wrapper then this is a valid wrapper, cache and return it
+        if (wrapper != null) {
+            PERIPHERAL_CACHE.put(tile, wrapper);
+            return wrapper;
+        }
+
+        return null;
 	}
 
+    private static boolean isLuaPeripheral(Object instance) {
+        return instance.getClass().isAnnotationPresent(LuaPeripheral.class);
+    }
 }
