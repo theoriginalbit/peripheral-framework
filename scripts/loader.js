d6796136aba6ccc0f41e46aa9198056e5fabfb94
AliquotMesozoic <Michael@Michaels-MacBook-Pro.local>
2014-07-20 22:31:50 -0400

    Restructure and index

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,11 +1,11 @@
 var width = Math.floor(window.outerWidth/2 - 30);
 
-$.get("withframework.java", function(code){ //With peripheral framework
+$.get("misc/withframework.java", function(code){ //With peripheral framework
     $("#withframework").text(code);
     hljs.highlightBlock(document.getElementById("withframework"));
 });
 
-$.get("withoutframework.java", function(code){ //Without peripheral framework
+$.get("misc/withoutframework.java", function(code){ //Without peripheral framework
     $("#withoutframework").text(code);
     hljs.highlightBlock(document.getElementById("withoutframework"));
 });
